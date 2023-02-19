6628bf7786e7ca6c259559cecf197f920d3439b9
src alt <52785691+KoldKardio@users.noreply.github.com>
2023-02-19 15:59:54 +0545

    Add files via upload

--- demo.gd
+++ demo.gd
@@ -1,6 +1,9 @@
 extends Node
 
 onready var startMenu = $Control/startmenu
+onready var newsWindow = $Control/newsBox
+onready var mil_window = $Military/window
+onready var res_window = $Resource/window
 
 func _ready():
 	startMenu.visible = false
@@ -22,6 +25,7 @@ func _on_start_pressed():
 func _on_news_pressed():
 	print("this is news")
 	
+	newsWindow.visible = true
 	# close startmenu
 	menuClose()
 	
@@ -31,6 +35,8 @@ func _on_news_pressed():
 func _on_military_pressed():
 	print("this is military")
 	
+	mil_window.visible = true
+	
 	# close startmenu
 	menuClose()
 	
@@ -39,6 +45,8 @@ func _on_military_pressed():
 # startmenu - resource hub
 func _on_resource_pressed():
 	print("this is resource")
+	
+	res_window.visible = true
 	
 	# close startmenu
 	menuClose()
@@ -51,5 +59,59 @@ func _on_social_pressed():
 	
 	# close startmenu
 	menuClose()
+	
+	pass
+
+# news window variables
+onready var news_title = $Control/newsBox/newsTitle
+onready var news_image = $Control/newsBox/newsImg
+onready var news_info = $Control/newsBox/newsInfo
+#
+# old data
+onready var dataOld = [news_title.text, news_image.texture, news_info.text]
+
+# news window-next button
+func _on_next_pressed():
+#	print("you pressed next in news window!")
+	
+	# change data new
+	news_title.text = "Putin's Choice Over Peace!"
+	news_image.texture = load("res://assets/war.png")
+	news_info.text = "Russia's leader, Vladimir Vladimirovich Putin, has made his firm decision to begin war against Ukrainian forces."
+	
+	# show previous btn
+	$Control/newsBox/previous.visible = true
+	
+	# hide next btn
+	$Control/newsBox/next.visible = false
+	
+	pass
+
+
+func _on_previous_pressed():
+	
+	# reverse data
+	news_title.text = dataOld[0]
+	news_image.texture = dataOld[1]
+	news_info.text = dataOld[2]
+	
+	# hide previous btn
+	$Control/newsBox/previous.visible = false
+	
+	# show next btn
+	$Control/newsBox/next.visible = true
+	
+	
+	pass
+
+# military window option #
+func _on_view_pressed():
+	
+	$Military/Donetsk.visible = true
+	
+	pass
+
+func _on_counter_pressed():
+	$Military/Donetsk/ConfirmationDialog.visible = true
 	
 	pass
